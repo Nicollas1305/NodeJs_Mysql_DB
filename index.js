7318ba1c49e6dc98d4e8aa298321e69c992521e1
Nicollas Lima <cartola1320@gmail.com>
2022-10-28 02:16:59 -0300

    Updates

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyparser = require('body-parser');
+const { connection } = require('mongoose');
 var app = express();
 //Configuring express server
 app.use(bodyparser.json());
 
 //MySQL details
 var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
+    port: 3306,
     user: 'root',
     password: 'Banana1305*',
     database: 'sakila',
-    multipleStatements: true
 });
 
+mysqlConnection.connect(function(err){
+    if(err) throw err;
+    console.log("Connected!!!")
+})
 
-mysqlConnection.connect((err)=> {
+
+/*mysqlConnection.connect((err)=> {
     if(!err)
     console.log('Connection Established Successfully');
     else
     console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
-});
+});*/
 
 //Establish the server connection
 //PORT ENVIRONMENT VARIABLE
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3306;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
